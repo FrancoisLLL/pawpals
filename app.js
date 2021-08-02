b56ec0f1f8e39992c65ba57109d5b91d9aae0c08
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-08-02 17:13:40 +0200

    create pet model, search result route and add pet form

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
+const hbs = require("hbs")
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const petsRouter = require('./routes/pets')
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials")
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', petsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/pets.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const uploader = require("../config/cloudinary");
+const router = express.Router();
+const Pet = require("../models/pets")
+const petType = require("../models/pets")
+
+router.get("/find-pawpal", (req, res) => {
+    Pet.find()
+    .then((response) => {
+        res.render("pets/petList.hbs", {
+            pet: response,
+            css: ["style.css", 'pets.css']
+        });
+    })
+    .catch((error) => {
+        next(error)
+    })
+})
+
+
+router.get("/add-pet", (req, res, next) => {
+    res.render("pets/addPet.hbs")
+})
+
+console.log(typeof petType)
+
+router.post("/", uploader.single("picture"), (req, res, next) => {
+    const { name } = req.body;
+
+    const newPet = { name};
+
+    if ( req.file !== undefined) {
+        newPet.picture = req.file.path
+    }
+
+    Pet.create(newPet)
+    .then((pet) => {
+        console.log(pet)
+        res.redirect("/");
+    })
+    .catch((error) => {
+        next(error)
+    })
+})
+
+module.exports = router
