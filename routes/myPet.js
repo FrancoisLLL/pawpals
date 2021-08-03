528b1bf2298c836f7c98ad16aeca9c1bcb8706d5
Francois L <francoisly@ymail.com>
2021-08-03 17:03:25 +0200

    add picture management

--- routes/myPet.js
+++ routes/myPet.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Pet = require("../models/Pet");
 const User = require("../models/User");
 
+const fileUploader = require('../config/cloudinary');
 
 
 // console.log(Pet.schema.path("preferredEnvironment.0").enumValues)
@@ -45,7 +46,6 @@ router.get("/add-pet", (req, res, next) => {
         type: Pet.schema.path('type').enumValues,
         gender: Pet.schema.path('gender').enumValues,
         environment : Pet.schema.path('preferredEnvironment.0').enumValues
-
     })
 })
 
@@ -71,10 +71,10 @@ router.get("/add-pet", (req, res, next) => {
 
 
 // ADD PET FORM WITHOUT PICTURE
-router.post("/add-pet", (req, res, next) => {
+router.post("/add-pet", fileUploader.single("picture"), (req, res, next) => {
     let pet = req.body
     pet.owner = req.session.currentUser._id
-
+    pet.picture = req.file.path;
     Pet.create(pet)
     .then((petData) => {
         console.log(petData)
