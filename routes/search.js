a3f6e240674e8965407f55e161d0319414bd1c25
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-08-04 19:03:18 +0200

    before leaving for the day

--- routes/search.js
+++ routes/search.js
@@ -7,45 +7,64 @@ const requireAuth = require("../middlewares/auth")
 const ObjectId = require('mongoose').Types.ObjectId;
 
 // SEARCH
-router.get("/:id/search-playmate", requireAuth, async (req, res, next) => {
-    try {
-        const foundPet = await Pet.findById(req.params.id);
-        if(!req.session.currentPet) req.session.currentPet = { _id : foundPet._id};
-        console.log("CURRENT PET SESSION HAS STARTED", req.session)
+// router.get("/:id/search-playmate", requireAuth, async (req, res, next) => {
+//     try {
+//         const foundPet = await Pet.findById(req.params.id);
+//         if(!req.session.currentPet) req.session.currentPet = { _id : foundPet._id};
+//         console.log("CURRENT PET SESSION HAS STARTED", req.session)
 
-        Pet.find({
-                owner: {
-                    $ne: new ObjectId(req.session.currentUser._id)
-                }
-            })
-            .then((response) => {
-                res.render("pets/searchForm.hbs", {
-                    type: Pet.schema.path('type').enumValues,
-                    time: Pet.schema.path('time.0').enumValues,
-                    css: ["style", "search"],
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-                next(error)
-            })
-    } catch { error => console.log(error) }
-})
+//         Pet.find({
+//                 owner: {
+//                     $ne: new ObjectId(req.session.currentUser._id)
+//                 }
+//             })
+//             .then((response) => {
+//                 res.render("pets/searchForm.hbs", {
+//                     type: Pet.schema.path('type').enumValues,
+//                     time: Pet.schema.path('time.0').enumValues,
+//                     css: ["style", "search"],
+//                 });
+//             })
+//             .catch((error) => {
+//                 console.log(error);
+//                 next(error)
+//             })
+//     } catch { error => console.log(error) }
+// })
 
 
-router.get("/search/filter", async (req, res, next) => {
-    await Pet.find(req.query)
-    .then(() => {
-        console.log(req.query);
+router.get("/search/filter", (req, res, next) => {
+    Pet.find(req.query)
+    .then((response) => {
         res.render("pets/searchForm.hbs", {
-                    type: Pet.schema.path('type').enumValues,
-                    time: Pet.schema.path('time.0').enumValues,
-                    css: ["style", "search"],
-                    scripts: ["search"]
+            type: Pet.schema.path('type').enumValues,
+            time: Pet.schema.path('time.0').enumValues,
+            size: Pet.schema.path('size').enumValues,
+            css: ["style", "search"],
+            scripts: ["search"]
         })
     })
     .catch(error => next(error))
 })
+
+// router.get("/search/filter", (req, res, next) => {
+//     Pet.find(req.query)
+//     .then((response) => {
+//         console.log(req.query);
+//         res.render("pets/searchForm.hbs", {
+//             pet: response,
+//             type: Pet.schema.path('type').enumValues,
+//             time : Pet.schema.path('time.0').enumValues,
+//             css: ["style", "search"],
+//             scripts: ["search"],
+//         });
+        
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//         next(error)
+//     })
+// })
 
 
 router.get("/:id/search-playmate", requireAuth, async (req, res, next) => {
@@ -62,6 +81,7 @@ router.get("/:id/search-playmate", requireAuth, async (req, res, next) => {
                     pet: response,
                     type: Pet.schema.path('type').enumValues,
                     time: Pet.schema.path('time.0').enumValues,
+                    size: Pet.schema.path('size').enumValues,
                     css: ["style", "search"],
                     scripts: ["search"]
                 });
