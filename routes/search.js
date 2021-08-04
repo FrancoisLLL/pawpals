e6cc4d59c0592aee370910cbdd790c14afb22ac2
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-08-04 12:42:34 +0200

    add some styling to search" "

--- routes/search.js
+++ routes/search.js
@@ -7,24 +7,42 @@ const ObjectId = require('mongoose').Types.ObjectId;
 
 // SEARCH
 router.get("/search", (req, res, next) => {
-    Pet.find({
-            owner: {
-                $ne: new ObjectId(req.session.currentUser._id)
-            }
-        })
+    Pet.find(
+        { owner: { $ne: new ObjectId(req.session.currentUser._id) } }
+        )
         .then((response) => {
             res.render("pets/searchForm.hbs", {
                 pet: response,
                 type: Pet.schema.path('type').enumValues,
                 time : Pet.schema.path('time.0').enumValues,
                 css: ["style", "search"],
-                scripts: ["search"]
+                scripts: ["search"],
+                bootstrap: `<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">`
             });
         })
         .catch((error) => {
             console.log(error);
             next(error)
         })
+})
+
+router.get("/search/filter", (req, res, next) => {
+    Pet.find(req.query)
+    .then((response) => {
+        console.log(req.query);
+        res.render("pets/searchForm.hbs", {
+            pet: response,
+            type: Pet.schema.path('type').enumValues,
+            time : Pet.schema.path('time.0').enumValues,
+            css: ["style", "search"],
+            scripts: ["search"],
+        });
+        
+    })
+    .catch((error) => {
+        console.log(error);
+        next(error)
+    })
 })
 
 router.get("/search/:id", (req, res, next) => {
